fa51d0f905b88317d3b443bc5ca28ad439565005
Nikita Skovoroda <chalkerx@gmail.com>
2024-08-17 12:25:06 +0400

    test: recheck that we are getting the expected .buffer

--- tests/other.test.js
+++ tests/other.test.js
@@ -14,14 +14,20 @@ class TestArray extends Uint16Array {
   }
 }
 
-const checkInstance = (instance) => {
+const arr = new TestArray(10)
+
+const checkInstance = (instance, fill = true) => {
   assert.equal(instance.constructor.name, 'TestArray')
   assert.equal(instance.constructor, TestArray)
   assert.equal(instance.hello(), 'hi there')
   assert.equal(Object.getPrototypeOf(instance), TestArray.prototype)
+  if (fill) {
+    for (let i = 0; i < instance.length; i++) assert.notEqual(instance[i], 1)
+    // eslint-disable-next-line @exodus/mutable/no-param-reassign-prop-only
+    for (let i = 0; i < instance.length; i++) instance[i] = 1
+  }
 }
 
-const arr = new TestArray(10)
 assert.equal(TestArray.instances, 1)
 
 const mapped = arr.map((_, i) => i * 10)
@@ -30,7 +36,9 @@ checkInstance(mapped)
 
 const subarray = arr.subarray(0)
 assert.equal(TestArray.instances, 3)
-checkInstance(subarray)
+checkInstance(subarray, false) // same .buffer per spec
+assert.equal(subarray.buffer, arr.buffer)
+assert.equal(subarray.byteOffset, arr.byteOffset)
 
 const sliced = arr.slice(0)
 assert.equal(TestArray.instances, 4)
@@ -43,7 +51,9 @@ assert.equal(filtered.length, 4)
 
 const from = TestArray.from([2 ** 15, 3])
 assert.equal(TestArray.instances, 6)
-checkInstance(from)
+checkInstance(from, false) // don't want to mutate
 assert.equal(from.length, 2)
 assert.equal(from[0], 2 ** 15)
 assert.equal(from[1], 3)
+
+for (let i = 0; i < arr.length; i++) assert.notEqual(arr[i], 1)
